a1384977f103b954ff42cc5a485e054e89dcbfa6
liu lang yi ge <1909493019@qq.com>
2018-08-09 15:24:36 +0800

    first exprience

---
+++ README.md
@@ -0,0 +1,16 @@
+# dva 初次接触
+
+## 已经内置的常用依赖
+
+ - 内置`react` `react-dom` `react-router-dom` `propTypes`
+
+## 与常用的react的开发比较
+
+ - route文件夹中,放置的路由页面(在route.js)中引入配置,结构更加清晰
+ - model的,使用namespace,state，reducers来类似原来的分离的reducer文件
+ - dva的初始化,传入整个App的state,类似于原来在最上层传递store
+ - 定义好model后,需要在入口,使用model()方法来注册一下
+ - 在需要使用状态管理的地方,使用connect的方法,依旧是高阶组件的用法,能够进行属性和方法的过滤
+ - 在组件中,手动dispatch个actions,来对状态进行管理
+
+
--- src/index.js
+++ src/index.js
@@ -2,13 +2,30 @@ import dva from 'dva';
 import './index.css';
 
 // 1. Initialize
-const app = dva();
+// const app = dva();
+
+
+const app = dva({
+      initialState: {
+        products: [
+          {
+            name: 'dva',
+            id: 1,
+            key:1
+        },{
+            name: 'antd',
+            id: 2,
+            key:2
+    }],
+  },
+});
 
 // 2. Plugins
 // app.use({});
 
 // 3. Model
 // app.model(require('./models/example').default);
+app.model(require('./models/products').default);
 
 // 4. Router
 app.router(require('./router').default);
